012672291dea268b7a47898ff4001ee3e45e43f7
Packeins <esteban9696e.c@gmail.com>
2026-06-04 20:40:12 -0500

    Añadir capturas de evidencia y finalizar implementación

--- src/main/java/edu/udla/integracion/progreso2/Progreso2Application.java
+++ src/main/java/edu/udla/integracion/progreso2/Progreso2Application.java
@@ -2,6 +2,11 @@ package edu.udla.integracion.progreso2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 
 @SpringBootApplication
 public class Progreso2Application {
@@ -10,4 +15,36 @@ public class Progreso2Application {
         SpringApplication.run(Progreso2Application.class, args);
     }
 
+    // Declarar colas
+    @Bean
+    public Queue billingQueue() {
+        return new Queue("billing.queue", true);
+    }
+
+    @Bean
+    public Queue notificationsQueue() {
+        return new Queue("notifications.queue", true);
+    }
+
+    @Bean
+    public Queue analyticsQueue() {
+        return new Queue("analytics.queue", true);
+    }
+
+    // Declarar Exchange Fanout
+    @Bean
+    public FanoutExchange appointmentsExchange() {
+        return new FanoutExchange("appointments.events");
+    }
+
+    // Conectar las colas al Exchange Fanout
+    @Bean
+    public Binding bindingNotifications(Queue notificationsQueue, FanoutExchange appointmentsExchange) {
+        return BindingBuilder.bind(notificationsQueue).to(appointmentsExchange);
+    }
+
+    @Bean
+    public Binding bindingAnalytics(Queue analyticsQueue, FanoutExchange appointmentsExchange) {
+        return BindingBuilder.bind(analyticsQueue).to(appointmentsExchange);
+    }
 }
